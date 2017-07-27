8643293900031f8c2b2a1558f8a124e311198928
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-28 07:14:44 +0900

    First commit

---
+++ Kalendar/Kalendar.h
@@ -0,0 +1,19 @@
+//
+//  Kalendar.h
+//  Kalendar
+//
+//  Created by Yudai.Hirose on 2017/07/28.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kalendar.
+FOUNDATION_EXPORT double KalendarVersionNumber;
+
+//! Project version string for Kalendar.
+FOUNDATION_EXPORT const unsigned char KalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kalendar/PublicHeader.h>
+
+
