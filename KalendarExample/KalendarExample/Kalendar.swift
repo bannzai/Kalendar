59ca00a62506b269d173f35d8bc3f78be8c75c96
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-07-28 07:16:35 +0900

    Add Kalendar and edit organizer

---
+++ KalendarExample/KalendarExample/Kalendar.swift
@@ -0,0 +1,13 @@
+//
+//  Kalendar.swift
+//  KalendarExample
+//
+//  Created by Yudai.Hirose on 2017/07/28.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+public class Kalendar: UIView {
+    
+}
